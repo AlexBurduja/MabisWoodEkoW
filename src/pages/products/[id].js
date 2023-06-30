41a34b901a07430ada6b46e364ada1b5af779fbf
Alex Burduja <burduja_alex@yahoo.com>
2023-07-01 00:01:29 +0300

    .

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import {  useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { doc, getDoc} from "firebase/firestore";
 import { db } from "../../../firebase-config";
 import Loading from "../../components/reusableComponents/Loading";
 import Header from '@/components/reusableComponents/Header';
 import { Footer } from '@/components/reusableComponents/Footer';
 import TopScrollProgress from '@/components/reusableComponents/TopScrollProgress';
+import { IoIosCall } from 'react-icons/io';
+import { BsPinMapFill, BsWhatsapp } from 'react-icons/bs';
+import { FiMail } from 'react-icons/fi';
+import Link from 'next/link';
 
 function Id() {
 
@@ -24,9 +28,9 @@ function Id() {
           const data = await getDoc(ref)
       
         setproductDetails(data.data())
-        setLoading(false)
         }
         
+        setLoading(false)
         getProduct();
       }, [id])
 
@@ -55,19 +59,69 @@ function Id() {
     }
   }, []);
 
+  const [modal, setModal] = useState(false)
+
+  function handleModal() {
+    setModal(!modal)
+
+    modal === false ? document.body.classList.add('modal-open') : document.body.classList.remove('modal-open')
+  }
+
+  const phoneNumber = '+40721648424';
+  const whatsappLink = `https://wa.me/${phoneNumber}`;
+
   return (
     <>
     {loading ? <Loading /> : 
     <>
+    <section className='sectionCenter'>
     <TopScrollProgress />
       <Header />
-    <section className='sectionCenter'>
+      
+      <h1 className='productTitle'>{productDetails.title}</h1>
+      
+
       <div className="product">
 
         <div className="productDescription">
           <p>{productDetails.description}</p>
           <p>{productDetails.kg} Kg </p>
+          <motion.button
+                className='quantityButton'
+                onClick={handleModal}
+                whileHover={{scale: 1.1}}
+                whileTap={{scale: 0.9}}>
+                Negociaza-ti cantitatea.
+          </motion.button>
+
         </div>
+          <AnimatePresence >
+      {modal && (
+        <motion.div 
+        initial={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}
+        transition={{duration: 0.3}}
+        className='modal'
+        >
+          <div className='modalOverlay' onClick={handleModal}></div>
+            <div className='modal-content modal1 '>
+              <div>
+                <h1>Contacteaza echipa Mabis Wood Eko alegand o optiune de mai jos sau prin completarea formularului din zona <Link href='/contact'>Contact.</Link></h1>
+              </div>
+
+              <div>
+              <div className="productPageModal">
+              <p><span> <IoIosCall /> </span> <a href="tel:+40721648424">+40721648424</a> </p>
+              <p><span><BsWhatsapp /> </span> <a href={whatsappLink}>+40721648424</a> </p>
+              <p><span> <FiMail /> </span> <a href="mailto:dan@mabiswood.ro">dan@mabiswood.ro</a> </p>
+              <p><span> <BsPinMapFill /> </span> <a href="https://goo.gl/maps/NrauAUNSKYqhu74E8" target='_blank'> Sat. Bogati, Comuna. Bogati, Strada. Alunis, Nr. 190B, Judetul. Arges</a></p>
+            </div>
+              </div>
+            </div>
+        </motion.div>
+      )}
+      </AnimatePresence>
         
         <div className="product-img">
           <motion.img
@@ -78,16 +132,13 @@ function Id() {
 
         <div className="productInfo">
             <p>Works best with a thermal plant.</p>
-            <h1>{productDetails.title}</h1>
             <p>{productDetails.price} {productDetails.currency}</p>
-            {/* <Link href='/#product'> */}
               <motion.button
               onClick={handleClick}
                 whileHover={{scale: 1.1}}
                 whileTap={{scale: 0.9}}>
                 Back to all products
               </motion.button>
-            {/* </Link> */}
         </div>
       
       </div>
