8f0d61b407882db7d7a392cd98964f0b104d5cb1
Alex Burduja <burduja_alex@yahoo.com>
2024-05-14 00:06:11 +0300

    testing testing

--- src/pages/ig.js
+++ src/pages/ig.js
@@ -13,37 +13,34 @@
 
   ///redirect uri
   ///https://www.mabiswoodeko.vercel.app/login
+
+//   curl -X POST \
+//   https://api.instagram.com/oauth/access_token \
+//   -F client_id=822508686597578 \
+//   -F client_secret=375f3282b89aebc5e85cae3df69efc6f \
+//   -F grant_type=authorization_code \
+//   -F redirect_uri=https://www.mabiswoodeko.vercel.app/login \
+//   -F code=AQDo6a9ZxyHuYLbzIWYZZ3JuZsSDW8MlwpirpV8TzWdWXihPbO-WbDDz_44J0LMW4ymnFrW89Rt7s3x1JPTRy77QZ6jCMb3KjcwOsAaWuuMr8c6gBY1trYmSolLhgmwXx5rcJmhgsvj8Z5PQJ7xKmNDEN_TTx89resfAFpI8C5zGtXHPMdZofrrrVugdbHwWkA5Znqoc15PY4-YIRwyJ4ntwyf0Pqor6F9TNIkhj4V5Eug
         
         const fetchAccessToken = async () => {
-            const clientId = '822508686597578';
-            const clientSecret = '375f3282b89aebc5e85cae3df69efc6f';
-            const redirectUri = 'https://www.mabiswoodeko.vercel.app/login';
-            const code = 'AQDo6a9ZxyHuYLbzIWYZZ3JuZsSDW8MlwpirpV8TzWdWXihPbO-WbDDz_44J0LMW4ymnFrW89Rt7s3x1JPTRy77QZ6jCMb3KjcwOsAaWuuMr8c6gBY1trYmSolLhgmwXx5rcJmhgsvj8Z5PQJ7xKmNDEN_TTx89resfAFpI8C5zGtXHPMdZofrrrVugdbHwWkA5Znqoc15PY4-YIRwyJ4ntwyf0Pqor6F9TNIkhj4V5Eug';
-    
-            try {
-                const response = await fetch('https://api.instagram.com/oauth/access_token', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/x-www-form-urlencoded'
-                    },
-                    body: new URLSearchParams({
-                        client_id: clientId,
-                        client_secret: clientSecret,
-                        grant_type: 'authorization_code',
-                        redirect_uri: redirectUri,
-                        code: code
-                    })
-                });
-    
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log(data); // Handle the response data here
-                } else {
-                    console.error('Failed to fetch access token');
-                }
-            } catch (error) {
-                console.error('Error fetching access token:', error);
-            }
+            const body = new FormData
+            body.append("client_id", "822508686597578")
+            body.append("", "\\")
+            body.append("client_secret", "375f3282b89aebc5e85cae3df69efc6f")
+            body.append("", "\\")
+            body.append("grant_type", "authorization_code")
+            body.append("", "\\")
+            body.append("redirect_uri", "https://www.mabiswoodeko.vercel.app/login")
+            body.append("", "\\")
+            body.append("code", "AQDo6a9ZxyHuYLbzIWYZZ3JuZsSDW8MlwpirpV8TzWdWXihPbO-WbDDz_44J0LMW4ymnFrW89Rt7s3x1JPTRy77QZ6jCMb3KjcwOsAaWuuMr8c6gBY1trYmSolLhgmwXx5rcJmhgsvj8Z5PQJ7xKmNDEN_TTx89resfAFpI8C5zGtXHPMdZofrrrVugdbHwWkA5Znqoc15PY4-YIRwyJ4ntwyf0Pqor6F9TNIkhj4V5Eug")
+            
+            fetch("https://api.instagram.com/oauth/access_token", {
+              body,
+              headers: {
+                "Content-Type": "multipart/form-data"
+              },
+              method: "POST"
+            })
         };
         
 
