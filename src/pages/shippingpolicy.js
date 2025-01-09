98e6d56432d44e3a6616e82de2de490c9a0579a6
Alex Burduja <burduja_alex@yahoo.com>
2025-01-09 21:41:51 +0200

    .

---
+++ src/pages/shippingpolicy.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ShippingPolicy() {
+
+
+  return (
+    <div className='divPrivacy'>
+      <iframe className='iframePrivacy' title='privacyFrame' src='https://firebasestorage.googleapis.com/v0/b/pelets-af6eb.appspot.com/o/legal%2FShipping%20Policy.pdf?alt=media&token=21e476e0-49cf-4760-a29f-ad47545d8470'/>
+    </div>
+  )
+}
