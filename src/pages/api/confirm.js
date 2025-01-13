f02be9dc03674f30bd5915c900850a3f2d227853
Alex Burduja <burduja_alex@yahoo.com>
2025-01-13 18:12:44 +0200

    .

--- src/pages/api/confirm.js
+++ src/pages/api/confirm.js
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-      // Handle IPN data from Netopia
       console.log('IPN Received:', req.body);
-      res.status(200).send('OK');
+      res.status(200).send('SUCCESS');
     } else {
       res.status(405).send('Method not allowed');
     }
