b2591af36a00da83b5ebd1c0b79a351a053bc1e1
Alex Burduja <burduja_alex@yahoo.com>
2025-01-18 00:28:15 +0200

    .

--- src/pages/api/confirm.js
+++ src/pages/api/confirm.js
@@ -1,9 +1,20 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-      console.log('IPN Received:', req.body);
+    try {
+      const { order, payment } = JSON.parse(req.body);
 
-      res.status(200).json({ errorCode: 0 });
+      if (payment.status === 3) {
+        // Payment successful
+        return res.redirect(`/success?orderId=${order.orderID}`);
+      } else {
+        // Payment failed or canceled
+        return res.redirect(`/cancel?orderId=${order.orderID}`);
+      }
+    } catch (error) {
+      console.error('Error processing payment:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   } else {
-      res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
