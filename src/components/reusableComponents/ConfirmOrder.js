ad2566ea0a8ddf06d19920412beff6ef16e5695a
Alex Burduja <burduja_alex@yahoo.com>
2025-01-18 20:37:44 +0200

    .

--- src/components/reusableComponents/ConfirmOrder.js
+++ src/components/reusableComponents/ConfirmOrder.js
@@ -5,11 +5,6 @@ export async function confirmOrder({ order, payment }) {
     // Here you can handle order processing logic. For example:
     console.log('Processing order:', order);
     console.log('Payment status:', payment.status);
-  
-    // You can validate the order and payment here
-    if (payment.status !== 'SUCCESS') {
-      throw new Error('Payment failed');
-    }
 
     await setDoc(doc(db, `orders/${order.orderId}`), {
         order: order,
