5642914632fcc08341e0ab6268f636df9e2fbbd9
Alex Burduja <burduja_alex@yahoo.com>
2025-01-18 16:19:11 +0200

    .

--- src/pages/api/confirm.js
+++ src/pages/api/confirm.js
@@ -1,14 +1,21 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      console.log("IPN RECEIVED:", req.body)
-      // if (payment.status === 3) {
-      //   // Payment successful
-      //   return res.redirect(`/success?orderId=${order.orderID}`);
-      // } else {
-      //   // Payment failed or canceled
-      //   return res.redirect(`/cancel?orderId=${order.orderID}`);
-      // }
+      const { order, payment } = req.body;
+
+      console.log(payment.status);
+
+      // Redirect logic (handled via headers for the user)
+      if (payment.status === 3) {
+        // Payment successful
+        res.writeHead(302, { Location: `/success?orderId=${order.orderID}` });
+      } else {
+        // Payment failed or canceled
+        res.writeHead(302, { Location: `/cancel?orderId=${order.orderID}` });
+      }
+
+      // Finalize response with a 200 status for Netopia
+      res.status(200).json({ errorCode: 0 });
     } catch (error) {
       console.error('Error processing payment:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
