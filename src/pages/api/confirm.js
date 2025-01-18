e9436b786051125104bb183a84c92a569b279f01
Alex Burduja <burduja_alex@yahoo.com>
2025-01-18 15:57:22 +0200

    .

--- src/pages/api/confirm.js
+++ src/pages/api/confirm.js
@@ -1,15 +1,14 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const { order, payment } = JSON.parse(req.body);
-
-      if (payment.status === 3) {
-        // Payment successful
-        return res.redirect(`/success?orderId=${order.orderID}`);
-      } else {
-        // Payment failed or canceled
-        return res.redirect(`/cancel?orderId=${order.orderID}`);
-      }
+      console.log("IPN RECEIVED:", req.body)
+      // if (payment.status === 3) {
+      //   // Payment successful
+      //   return res.redirect(`/success?orderId=${order.orderID}`);
+      // } else {
+      //   // Payment failed or canceled
+      //   return res.redirect(`/cancel?orderId=${order.orderID}`);
+      // }
     } catch (error) {
       console.error('Error processing payment:', error);
       return res.status(500).json({ error: 'Internal Server Error' });
