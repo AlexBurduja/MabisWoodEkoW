4a11ba65ceb6673f749eb0593761ebb2d1bd4644
Alex Burduja <burduja_alex@yahoo.com>
2025-01-19 12:32:52 +0200

    .

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,18 +1,41 @@
 'use client'
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation'
 import { db } from '../../firebase-config';
+import { getDatabase } from 'firebase/database'
+import { doc, getDoc } from 'firebase/firestore';
 
 function Success() {
+  const [order, setOrder] = useState({})
   const searchParams = useSearchParams();
 
   const orderId = searchParams.get('orderId')
   
+  useEffect(() => {
+    
+  const getOrder = async () => {
+    const ref = doc(db, `orders/${orderId}`)
   
-  const ref = ref(db, `orders/${orderId}`)
+    const data = await getDoc(ref)
+    setOrder(data.data())
+    console.log(order)
+  };
+
+  getOrder();
+  }, [])
+
+  useEffect(() => {
+    console.log(order)
+  }, [order])
+
 
   return (
     <div>
+      {order.payment.status === 3 ? 
+      "Payment was successfull" 
+      : 
+      "Payment was not successfull!"
+      }
       { orderId }
     </div>
   );
