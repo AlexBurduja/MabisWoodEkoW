0a81ccc02535e72d74e13eecd32541601cd56646
Alex Burduja <burduja_alex@yahoo.com>
2025-01-20 21:28:20 +0200

    .

--- src/components/reusableComponents/Map.js
+++ src/components/reusableComponents/Map.js
@@ -1,24 +1,47 @@
-import { useEffect } from 'react';
-import L from 'leaflet';
+import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import customIconUrl from '../../publicResources/logoMabisCerc.svg'
+// import 'leaflet/dist/leaflet.css';
+
+// Fix for Leaflet marker icons
+  import L from 'leaflet';
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png').default,
+    iconUrl: require('leaflet/dist/images/marker-icon.png').default,
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+  });
+
+  const customIcon = new L.Icon({
+    iconUrl: customIconUrl,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png').default,
+    shadowSize: [41, 41],
+  });
 
 export default function Map() {
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const map = L.map('map').setView([51.505, -0.09], 13);
+  return (
+    <div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ height: '500px', width: '100%' }}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]} icon={customIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <Marker position={[51.50, -0.09]} icon={customIcon}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
 
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
-
-      L.marker([51.505, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
-
-      return () => {
-        map.remove();
-      };
-    }
-  }, []);
-
-  return <div id="map" style={{ height: '500px', width: '100%' }} />;
+    <img src={customIconUrl} width={200} height={100}></img>
+    </div>
+  );
 }
